36a7fe7cc662f3fd65dbc3881121b4fc0eddff3c
kevintan-dev <me@kevintan-dev.com>
2023-08-25 08:00:43 +0200

    Add accor app id

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,4 +1,4 @@
 export const ethereumRecIdOffset = 27;
-export const apiUrl = "http://localhost:3000"
+export const apiUrl = "https://app.stg.dfns-frame.smart-chain.fr"
 export const dfnsHost = "https://api.dfns.ninja"
-export const dfnsAppId = "ap-6juj0-9k1pq-9gc8qkpscjj2qsso"
+export const dfnsAppId = "ap-6fuqd-uvcjq-9qm8trqr58pss5ao"
